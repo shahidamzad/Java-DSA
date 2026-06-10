88599354c778b9bad6b9e88f1c8f833d1df382c8
Your Name <shahidamzad07@gmail.com>
2026-06-10 16:25:33 +0530

    type casting

---
+++ basic/halfOfGivenInteger.java
@@ -0,0 +1,22 @@
+package basic;
+
+import java.util.Scanner;
+
+public class halfOfGivenInteger {
+    static void main(String[] args) {
+        // type casting for int to dubble --
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a num : ");
+        int a  = sc.nextInt() ;
+        double y = (double) a ;
+        System.out.println(y/2);
+
+        // type casting dubble to int --
+
+        System.out.print("Enter c num : ");59
+        double c = sc.nextDouble() ;
+        int z = (int) c ;
+        System.out.println(z);
+     }
+}
