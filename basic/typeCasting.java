654e5554d649436206293019bd1af432bc7e1971
Your Name <shahidamzad07@gmail.com>
2026-06-11 10:01:38 +0530

    tyoecasting

---
+++ basic/typeCasting.java
@@ -0,0 +1,22 @@
+package basic;
+
+public class typeCasting {
+    static void main(String[] args) {
+        // int / int = int
+
+        System.out.println(  5/2);
+
+        // double / int = double
+
+        System.out.println(5.0/2);
+
+        // int /double = doulble
+
+        System.out.println(5/2.0);
+
+        // double / bouble  = double
+
+        System.out.println(5.0/2.0);
+
+    }
+}
