6a35cc7872de5b975d6199e6e6136440c5b5f10b
Your Name <shahidamzad07@gmail.com>
2026-06-17 12:56:48 +0530

    Data types

---
+++ basic/dataTypes.java
@@ -0,0 +1,35 @@
+package basic;
+
+public class dataTypes {
+    static void main(String[] args) {
+        // bytes -128 to 127
+        byte num = 20;
+        System.out.println("enter a Byte : " + (num));
+
+        // short -32768 to 32767
+        short num2 = 32767;
+        System.out.println("enter a Short : " + (num2));
+
+        // int and Long
+        int num3 = 65535;
+        System.out.println("enter a Integer : " + (num3));
+        long num4 = 9223372036854775807L;
+        System.out.println("enter a Long :" + (num4));
+
+
+        // Float and Double
+        float num5 = 3.14f;
+        System.out.println("enter a Float : " + (num5));
+
+        double num6 = 3.14;
+        System.out.println("enter a Double : " + (num6));
+
+        // Char
+
+        char num7 = 'a';
+        System.out.println("enter a Character : " + (num7));
+        char num8 = 'A';
+        System.out.println("enter a Short : " + (num8));
+
+    }
+}
