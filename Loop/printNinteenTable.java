67117495232925b6df4c6976f6332079aebba0ab
Your Name <shahidamzad07@gmail.com>
2026-07-13 12:50:51 +0530

    task

---
+++ Loop/printNinteenTable.java
@@ -0,0 +1,12 @@
+package Loop;
+
+public class printNinteenTable {
+    static void main(String[] args) {
+        int i ;
+        for (i=19;i<=190;i++)
+            if(i%19==0){
+                System.out.println(i);
+
+            }
+    }
+}
