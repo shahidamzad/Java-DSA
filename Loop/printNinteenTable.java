f63972fa86843a488360cd085fbc97c3988044d5
Your Name <shahidamzad07@gmail.com>
2026-07-13 12:58:06 +0530

    19 table optimize rounds

--- Loop/printNinteenTable.java
+++ Loop/printNinteenTable.java
@@ -3,10 +3,19 @@ package Loop;
 public class printNinteenTable {
     static void main(String[] args) {
         int i ;
+     // better way to print tables
+     // take 10 rounds
+        for(i=19;i<=190; i +=19){
+            System.out.println(i);
+        }
+
+        // takes 171 rounds
         for (i=19;i<=190;i++)
             if(i%19==0){
                 System.out.println(i);
 
             }
+
+
     }
 }
