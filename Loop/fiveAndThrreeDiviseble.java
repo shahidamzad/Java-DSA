bb6eba61d6e7bdc3330654d676a845c9240f0db4
Your Name <shahidamzad07@gmail.com>
2026-07-13 12:45:29 +0530

    task

---
+++ Loop/fiveAndThrreeDiviseble.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class fiveAndThrreeDiviseble {
+    // for loop
+    static void main(String[] args) {
+        int i ;
+        for (i=1;i<=100;i++)
+            if(i% 3==0 && i%5==0){
+                System.out.println(i);
+
+            }
+    }
+}
