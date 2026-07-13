98b255e0c14e02a23aa18dfb8d4ee7c2890f32ce
Your Name <shahidamzad07@gmail.com>
2026-07-13 12:41:06 +0530

    basic or for loop

---
+++ Loop/printOddNum.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class printOddNum {
+    static void main(String[] args) {
+        // print odd num in java
+        int i ;
+        for(i=1;i<=100;i++){
+            if(i%2 !=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
