2344d221793c770223fa6ae2f890b687785fca58
Your Name <shahidamzad07@gmail.com>
2026-07-17 15:45:47 +0530

    triangle pattern method

---
+++ specialPattern/compositePattern/starTriangleVerticallyFlippedComposite.java
@@ -0,0 +1,20 @@
+package specialPattern.compositePattern;
+
+import java.util.Scanner;
+
+public class starTriangleVerticallyFlippedComposite {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(" Enter a number : ");
+        int n = input.nextInt();
+        for (int i = 1 ; i <= n ; i++ ) {
+            for (int j = 1 ; j <= n-i; j++ ) {
+                System.out.print(" " + " ");
+            }
+            for (int j = 1 ; j <= i ; j++ ) {
+                System.out.print( j + " " );
+            }
+            System.out.println();
+        }
+    }
+}
