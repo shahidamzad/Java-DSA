7ccab1df23939729d8cefcca65479c9edc069e69
Your Name <shahidamzad07@gmail.com>
2026-07-17 15:46:00 +0530

    triangle pattern method

---
+++ specialPattern/compositePattern/rhombusPattern.java
@@ -0,0 +1,22 @@
+package specialPattern.compositePattern;
+
+import java.util.Scanner;
+
+public class rhombusPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter  n : ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" "+ " ");
+            }
+            for (int j = 1; j <=n ; j++) {
+                System.out.print(j + " ");
+
+            }
+            System.out.println();
+        }
+    }
+}
