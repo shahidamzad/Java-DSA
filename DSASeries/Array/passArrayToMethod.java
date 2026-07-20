73c0fa0151086b2b2850ca9ba1badf02d4d07c8e
Your Name <shahidamzad07@gmail.com>
2026-07-20 12:24:05 +0530

    arr

---
+++ DSASeries/Array/passArrayToMethod.java
@@ -0,0 +1,17 @@
+package DSASeries.Array;
+
+public class passArrayToMethod {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70,80,90,100};
+        System.out.print(arr[0]);
+        System.out.println();
+
+        change(arr);
+
+        System.out.println(arr[0 ]);
+    }
+
+    public static void change(int[] arr) {
+         arr[0] = 50 ;
+    }
+}
