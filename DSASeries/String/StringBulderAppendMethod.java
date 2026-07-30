b6796a6467eccfc221f6ee5f115f810130f2199a
Your Name <shahidamzad07@gmail.com>
2026-07-30 13:31:32 +0530

    StringBuilder append method

---
+++ DSASeries/String/StringBulderAppendMethod.java
@@ -0,0 +1,24 @@
+package DSASeries.String;
+
+public class StringBulderAppendMethod {
+    static void main(String[] args) {
+        // append
+
+        StringBuilder sb = new StringBuilder("avc");
+        // integer
+        sb.append(89);
+        System.out.println(sb);
+        //charecter  of array
+        char[] ch = {'a','b'};
+        sb.append(ch);
+        System.out.println(sb);
+        // string
+        sb.append("sleksjjhgjdfhb");
+        System.out.println(sb);
+
+
+
+
+
+    }
+}
